f034bd569ea9e53a295a88a7f46239ef44f6b079
samuelsuu <uwaemesamuel5@gmail.com>
2024-10-29 14:55:56 +0100

    updated your darkmode

--- App.js
+++ App.js
@@ -1,6 +1,22 @@
 import React from "react";
-import Navigation from "./src/navigations/appNavigation";
+import { NavigationContainer, DefaultTheme, DarkTheme } from "@react-navigation/native";
+import Navigation from "./src/navigations/appNavigation"; // Adjust path if needed
+import { ThemeProvider, ThemeContext } from "./src/models/ThemeContext"; 
 
 export default function App() {
-  return <Navigation />;
+  return (
+    <ThemeProvider>
+      <ThemedApp />
+    </ThemeProvider>
+  );
 }
+
+const ThemedApp = () => {
+  const { isDarkMode } = React.useContext(ThemeContext);
+
+  return (
+    <NavigationContainer theme={isDarkMode ? DarkTheme : DefaultTheme}>
+      <Navigation />
+    </NavigationContainer>
+  );
+};
--- src/navigations/appNavigation.js
+++ src/navigations/appNavigation.js
@@ -1,20 +1,18 @@
+// src/navigations/appNavigation.js
 import React, { useState, useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import * as Font from "expo-font";
+import { Text } from "react-native";
 import Regs from "./Reg";
 import TodoNav from "./TodoBottomNav";
 import HomeScreen from "../Homescreen";
 import MovieFinderNav from "./MovieFinderNav";
-
-
-import { Text } from "react-native";
 import DarkMode from "../DarkMode";
 
 const Stack = createNativeStackNavigator();
 
-const appNavigation = () => {
+const AppNavigation = () => {
   const [fontsLoaded, setFontsLoaded] = useState(false);
 
   const loadFonts = async () => {
@@ -33,40 +31,35 @@ const appNavigation = () => {
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Reg"
-            component={Regs}
-            options={{ headerShown: false }}
-          />
-            <Stack.Screen
-            name="HomeScreen"
-            component={HomeScreen}
-            options={{ headerShown: false }}
-          />
-            <Stack.Screen
-            name="DarkMode"
-            component={DarkMode}
-            options={{ headerShown: false }}
-          />
-        
-        
-      
-             <Stack.Screen
-            name="TodoNav"
-            component={TodoNav}
-            options={{ headerShown: false }}
-          />
-           <Stack.Screen
-            name="MovieFinderNav"
-            component={MovieFinderNav}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Reg"
+          component={Regs}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="DarkMode"
+          component={DarkMode}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="TodoNav"
+          component={TodoNav}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="MovieFinderNav"
+          component={MovieFinderNav}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
     </GestureHandlerRootView>
   );
 };
 
-export default appNavigation;
+export default AppNavigation;
